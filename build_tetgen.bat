8b1236612e3b573610c3b53788041edd03e6d296
Man Poor <poormanslaserscanner@gmail.com>
2017-10-23 18:02:28 +0200

    * Fix build script for tetgen

--- build_tetgen.bat
+++ build_tetgen.bat
@@ -3,4 +3,6 @@ cd ..
 mkdir build\tetgen
 cd build\tetgen
 cmake -A x64 -T v141 -DCMAKE_CONFIGURATION_TYPES:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=..\..\install\tetgen ..\..\pmls\tetgen
-devenv /build "Release|x64" /project ALL_BUILD Project.sln 
+devenv /build "Release|x64" /project ALL_BUILD Project.sln
+
+
