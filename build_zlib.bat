0bb092c09f50b185d47e7834955282affd32b40c
Man Poor <poormanslaserscanner@gmail.com>
2017-11-09 06:56:58 +0100

    *Fix zlib build script

--- build_zlib.bat
+++ build_zlib.bat
@@ -2,7 +2,7 @@ call "%VC_ROOT%\vcvarsall.bat" x64
 cd ..
 mkdir build\zlib
 cd build\zlib
-cmake -A x64 -T v141 -DCMAKE_CONFIGURATION_TYPES:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=..\..\install\zlib ..\..\pmls\zlib
+cmake -A x64 -T v141 -DCMAKE_CONFIGURATION_TYPES:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH="%PMLS_INSTALL_DIR%\zlib" ..\..\iso2mesh_bin\zlib
 devenv /build "Release|x64" /project ALL_BUILD zlib.sln
 devenv /build "Release|x64" /project INSTALL zlib.sln
  
