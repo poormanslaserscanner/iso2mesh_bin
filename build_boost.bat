b7a52f932b78741ec2a932ce05434529c2d71324
Man Poor <poormanslaserscanner@gmail.com>
2017-11-09 06:56:25 +0100

    * Fix: build boost script

--- build_boost.bat
+++ build_boost.bat
@@ -2,6 +2,7 @@ call "%VC_ROOT%\vcvarsall.bat" x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd boost
 call bootstrap
-b2 variant=release link=static threading=multi address-model=64 --build-dir="..\..\build\boost\build" --stagedir="..\..\build\boost\build\stage"  --prefix="..\..\install\boost" --build-type=minimal stage install headers
+b2 -j8 variant=release link=static runtime-link=shared threading=multi address-model=64 --build-dir="..\..\build\boost\build" --stagedir="..\..\build\boost\build\stage"  --prefix="%PMLS_INSTALL_DIR%\boost" --build-type=minimal stage
+b2 -j8 variant=release link=static runtime-link=shared threading=multi address-model=64 --build-dir="..\..\build\boost\build" --stagedir="..\..\build\boost\build\stage"  --prefix="%PMLS_INSTALL_DIR%\boost" --build-type=minimal install
 
 
